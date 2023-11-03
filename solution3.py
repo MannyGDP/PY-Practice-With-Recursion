248105296b4ba20c765c9302f93853ccf1deaec1
Manny Delgado <mannygdp@gmail.com>
2023-11-02 22:18:03 -0700

    Part 3

--- solution3.py
+++ solution3.py
@@ -1,1 +1,14 @@
-# Write code for algorithm 3 below
+def fibonacci(n):
+    if n <= 0:
+        return "Invalid input. Please provide a positive integer."
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Test the function with n = 4
+n = 4
+result = fibonacci(n)
+print(result)
